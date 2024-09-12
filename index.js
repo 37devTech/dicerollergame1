16a1cb38ee9d0c1cceb5fca9c0eaaca7d1ab0f99
37devTech <neilhumphreytech@gmail.com>
2024-09-12 17:13:06 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var roll = Math.floor(Math.random()*6) +1;
+
+if (roll === 1)
+{
+    document.getElementById("img1").src = "dicepic1.png";
+}
+else if (roll === 2)
+{
+    document.getElementById("img1").src = "dicepic2.png";
+}
+else if (roll === 3)
+{
+    document.getElementById("img1").src = "dicepic3.png";
+}
+else if (roll === 4)
+{
+    document.getElementById("img1").src = "dicepic4.png";
+}
+else if (roll === 5)
+{
+    document.getElementById("img1").src = "dicepic5.png";
+}
+else if (roll === 6)
+{
+    document.getElementById("img1").src = "dicepic6.png";
+}
